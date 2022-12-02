ed998b550ff4201392fcb984b9fd395bf0e89889
Tom Bull <thomas.bull@metaswitch.com>
2022-12-02 15:21:04 +0000

    1-Dec: part 2

--- 1-dec/Program.cs
+++ 1-dec/Program.cs
@@ -6,10 +6,9 @@ class Program
         Console.WriteLine("1 Dec");
         string[] lines = System.IO.File.ReadAllLines(@"input.txt");
 
-        // Display the file contents by using a foreach loop.
-        //System.Console.WriteLine("Contents of input.txt = ");
-        int elf_max_total = 0;
-        int elf_max_number = 1;
+        int num_elves = 3; 
+        int[] elf_max_total = {0, 0, 0};
+        int[] elf_max_number = {-1, -1, -1};
         int elf_total = 0;
         int elf_number = 1;
         foreach (string line in lines)
@@ -20,16 +19,28 @@ class Program
             }
             else
             {
-                if (elf_total > elf_max_total)
+                for(int elf_index = 0; elf_index < num_elves; elf_index++)
                 {
-                    elf_max_number = elf_number;
-                    elf_max_total = elf_total;
+                    if (elf_total > elf_max_total[elf_index])
+                    {
+                        elf_max_number[elf_index] = elf_number;
+                        elf_max_total[elf_index] = elf_total;
+                        break;
+                    }
                 }
                 elf_number += 1;
                 elf_total = 0;
             }
         }
-        Console.WriteLine("Elf Number = " + elf_max_number);
-        Console.WriteLine("Elf calories = " + elf_max_total);
+        Console.WriteLine("Elf Number = " + elf_max_number[0] + " " + elf_max_number[1] + " " + elf_max_number[2]);
+        Console.WriteLine("Elf calories = " + elf_max_total[0] + " " + elf_max_total[1] + " " + elf_max_total[2]);
+
+        int elf_max_total_sum = 0;
+        foreach (int elf_total_sum in elf_max_total)
+        {
+            elf_max_total_sum += elf_total_sum;
+        }
+        Console.WriteLine("TOTAL = " + elf_max_total_sum);
+        
     }
 }
